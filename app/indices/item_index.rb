949a36f28d099a9811d9fc00da3cb2d3a84ad42d
IzotovDenis <izotov87@gmail.com>
2019-11-13 11:30:42 +1000

    add subtitle to index

--- app/indices/item_index.rb
+++ app/indices/item_index.rb
@@ -3,6 +3,7 @@ ThinkingSphinx::Index.define :item, :with => :active_record do
     # fields
     
     indexes title, :sortable => true
+    indexes subtitle
     indexes brand.title, :as => :brand, :sortable => true
     has created_at, updated_at
 end
